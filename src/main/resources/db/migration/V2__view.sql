697e5582cbf70e3eb635ab3a94fa373f3778999d
radovanradic <radicr@gmail.com>
2023-04-29 20:44:44 +0200

    Use generated view script

--- src/main/resources/db/migration/V2__view.sql
+++ src/main/resources/db/migration/V2__view.sql
@@ -1,13 +1,15 @@
-CREATE OR REPLACE JSON DUALITY VIEW STUDENT_SCHEDULE AS
-SELECT JSON{'student'  : s.name WITH UPDATE,
-            'studentId': s.id,
-            'schedule': [SELECT JSON {'class': (SELECT JSON{
-                                                    'name': c.name,
-                                                    'classID': c.id,
-                                                    'time': c.time WITH UPDATE,
-                                                    'room': c.room,
-                                                    'teacher': (SELECT JSON{'teachID': t.id, 'teacher': t.name} FROM teacher t WHERE t.id = c.teacher_id)
-                                                } FROM class c WITH UPDATE WHERE c.id = ssch.class_id),
-                                      'id': ssch.id
-                                      } FROM student_classes ssch WITH INSERT DELETE WHERE ssch.student_id = s.id]
-            } FROM student s WITH INSERT UPDATE DELETE;
+CREATE OR REPLACE JSON RELATIONAL DUALITY VIEW STUDENT_SCHEDULE AS
+SELECT JSON{
+        'studentId': STUDENT.id,
+    'student': STUDENT.name WITH UPDATE,
+'schedule': [SELECT JSON{
+'id': STUDENT_CLASSES.id,
+'class': (SELECT JSON{
+'classID': CLASS.id,
+'teacher': (SELECT JSON{
+'teachID': TEACHER.id,
+'teacher': TEACHER.name} FROM TEACHER WITH UPDATE WHERE CLASS.teacher_id = TEACHER.id),
+'room': CLASS.room,
+'time': CLASS.time,
+'name': CLASS.name WITH UPDATE} FROM CLASS WITH UPDATE WHERE STUDENT_CLASSES.class_id = CLASS.id)}
+    FROM STUDENT_CLASSES WITH INSERT UPDATE DELETE WHERE STUDENT.id = STUDENT_CLASSES.student_id]} FROM STUDENT WITH UPDATE INSERT DELETE;
